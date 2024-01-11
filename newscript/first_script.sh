46d6e853071450ab8369912a6aadca079928579b
mitya <milimon.one@gmail.com>
2024-01-12 06:49:53 +0700

    Izuchenie komandi ob'edineniya

---
+++ newscript/first_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+n1="Boss"
+x=5
+y=20
+z=2
+echo "Hello $n1"
+new=$(($x+$y/2-$z))
+echo $new
+ 
--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -5,3 +5,4 @@ read first_name
 echo what you last name
 read last_name
 echo WHY $first_name $last_name
+git push
